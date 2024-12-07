6778198bed1646dfa219342f77e70309cd631ba1
AngeloGagno <angelogagno@gmail.com>
2024-12-06 22:18:05 -0300

    Adicionando novas requisicoes

--- src/API_Data/Get/get_bookings_list.py
+++ src/API_Data/Get/get_bookings_list.py
@@ -4,7 +4,8 @@ from request import Request_Bookings_API
 class Booking:
     def __init__(self,data):
         self.data = data['data']
-
+        self.payments = data['data']['amounts']
+        
     def check_in_date(self):
         return self.data['stayDates']['arrival']
 
@@ -20,6 +21,18 @@ class Booking:
     def accommodation(self):
         return self.data['accommodation']['id']
     
-data = Request_Bookings_API().get_api_booking_description('23895641') 
-print(Booking(data).creation_date())
+    def sales_channel(self):
+        return self.data.get('salesChannel',{}).get('name','')
+    
+    def net_payment(self):
+        return self.payments['breakdown']['base']['net']
+
+    def tourism_tax(self):
+        return self.payments.get('taxes',{}).get('tourism',{}).get('net',0)
+    
+    def portal_comission(self):
+        return self.payments['commission']['portal']
+
+data = Request_Bookings_API().get_api_booking_description('23322808') 
+print(Booking(data).check_out_date())
 
