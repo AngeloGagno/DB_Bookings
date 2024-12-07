4363745d539864c4188ed7a62f688f0c18522853
AngeloGagno <angelogagno@gmail.com>
2024-12-06 21:14:33 -0300

    Criando a classe de Bookings e a requisicao

--- src/API_Data/Get/get_bookings_list.py
+++ src/API_Data/Get/get_bookings_list.py
@@ -1,3 +1,25 @@
 
-import requests
+from request import Request_Bookings_API
+
+class Booking:
+    def __init__(self,data):
+        self.data = data['data']
+
+    def check_in_date(self):
+        return self.data['stayDates']['arrival']
+
+    def check_out_date(self):
+        return self.data['stayDates']['departure']
+    
+    def creation_date(self):
+        return self.data['creationDate']
+
+    def status(self):
+        return self.data['status']
+    
+    def accommodation(self):
+        return self.data['accommodation']['id']
+    
+data = Request_Bookings_API().get_api_booking_description('23895641') 
+print(Booking(data).creation_date())
 
--- src/API_Data/Get/request.py
+++ src/API_Data/Get/request.py
@@ -4,28 +4,42 @@ import os
 
 
 class Request_Bookings_API:
-    def __init__(self,date_begin=None,date_end=None):
+
+    def __init__(self):
         self.apikey = self._apikey()    
-        self.header = self.header()
-        self.checkin_begin = date_begin
-        self.checkin_ends = date_end
+        self.header = self._header()
+
     def _apikey(self):
         load_dotenv()
         api = os.getenv('API_AVANTIO')
         return api
 
-    def header(self):
+    def _header(self):
         return {
         "accept": "application/json",
         "X-Avantio-Auth": self.apikey
     }
 
-    def get_api_response(self):
+    def _get_api_booking_list(self):
         status = requests.get('https://api.avantio.pro/pms/v2/bookings',headers=self.header)
         return status.status_code
     
-    def get_api_data(self):
-        return requests.get(fr'https://api.avantio.pro/pms/v2/bookings?arrivalDate_from={self.checkin_begin}&arrivalDate_to={self.checkin_ends}',headers = self.header).json()
-
-api = Request_Bookings_API('2024-12-01','2024-12-02')
-print(api.get_api_data())
+    def _get_api_booking_description(self):
+        status = requests.get('https://api.avantio.pro/pms/v2/bookings',headers=self.header)
+        return status.status_code
+    
+    def get_api_booking_description(self,id):
+        '''
+        id - Booking ID
+        '''
+        return requests.get(f'https://api.avantio.pro/pms/v2/bookings/{id}',headers=self.header).json()
+        
+    
+    def get_bookings_list(self,checkin_begin:str,checkin_ends:str):
+        '''
+        date_begin and date_end - Must be a string format(%Y-%m-%d) \n
+        %Y - Year \n
+        %m - Month \n
+        %d - Day 
+        '''     
+        return requests.get(fr'https://api.avantio.pro/pms/v2/bookings?arrivalDate_from={checkin_begin}&arrivalDate_to={checkin_ends}',headers = self.header).json()
