f2a7b2680750f946c5a184101667ed7bf0f81af5
angelogagno <angelogagno@gmail.com>
2025-02-10 16:29:33 -0300

    Atualizando o bookings update

--- src/Booking_Update.py
+++ src/Booking_Update.py
@@ -1,7 +1,7 @@
 from API_Data.get_bookings_description import get_booking_updates
 from Database.database import get_db,Base,engine
 from Database.commit_to_db import commit_data_on_db
-
+from Logs.send_email import email_credenciais,email
 
 def main():
     # Cria as tabelas no banco de dados, se ainda não existirem
@@ -9,6 +9,10 @@ def main():
     
     # Armazena as reservas no banco de dados
     commit_data_on_db(bookings=get_booking_updates(), db=get_db())
+
+    except Exception as e:
+
+        email(credenciais=email_credenciais(),erro=e)
 
 if __name__ == "__main__":
     main()
